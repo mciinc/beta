db04a4caee92aa4eb85fd0fe471a9399dc7c884e
mciinc <isabel@microconnectors.com>
2013-04-27 13:43:17 -0700

    Update send3.php

--- send3.php
+++ send3.php
@@ -1,13 +1,1 @@
-<?
-     foreach($_POST  as $key => $value){
-          $mess .=str_replace("_"," ",$key).": ".$value."\n";
-     }
-     
-     $to ="online@microconnectors.com";
-     
-     $subj = "Website Contact From";
-     
-     mail($to,$subj,$mess);
-     
-     header("Location: http://www.microconnectors.com/servlet/the-template/thankyou/Page");
-?>
+
